9a4bc83fb2beca1c9fa69379499a3fceef114536
a <a@HYONI>
2018-11-01 22:16:22 +0900

    WeatherDao

--- HyoniCrawling/src/main/java/com/hyoni/crawling/CrawlingController.java
+++ HyoniCrawling/src/main/java/com/hyoni/crawling/CrawlingController.java
@@ -1,13 +1,25 @@
 package com.hyoni.crawling;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CrawlingController {
 	@RequestMapping("/test")
-	public String testCrawlling() {
-		System.out.println("dddd");
+	public String testCrawlling(Model model) {
+		Document doc = null;
+		try {
+			doc = Jsoup.connect("https://datalab.naver.com/").get();
+			model.addAttribute("craw1", doc);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return "/test/test";
 	}
 }
--- HyoniCrawling/src/main/java/com/hyoni/crawling/HomeController.java
+++ HyoniCrawling/src/main/java/com/hyoni/crawling/HomeController.java
@@ -1,12 +1,21 @@
 package com.hyoni.crawling;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	public String index(){
+		HttpServletRequest req = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+		String ip = req.getHeader("X-FORWARDED-FOR");
+		if(ip == null)	ip = req.getRemoteAddr();
+		
+		System.out.println("hello : " + ip);
 		return "index";
 	}
 }
