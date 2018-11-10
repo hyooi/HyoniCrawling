34a32eef1b303c0fd32ebe05244e24f9b5ddb820
a <a@HYONI>
2018-11-10 14:09:56 +0900

    연관키워드 추가

--- HyoniCrawling/src/main/java/com/hyoni/crawling/controller/ChatbotController.java
+++ HyoniCrawling/src/main/java/com/hyoni/crawling/controller/ChatbotController.java
@@ -27,7 +27,7 @@ public class ChatbotController {
 	
 	@RequestMapping(value = "/keyboard", method = RequestMethod.GET)
 	public KeyboardVO keyboard() {
-		KeyboardVO keyboard = new KeyboardVO(new String[] {"안녕?", "날씨알려줘"});
+		KeyboardVO keyboard = new KeyboardVO(new String[] {"안녕", "도움말"});
 		
 		return keyboard;
 	}
@@ -37,8 +37,15 @@ public class ChatbotController {
 		String resContent;
 		
 		if ((reqContent.contains("안녕"))||(reqContent.contains("하이")||(reqContent.contains("hi")))){
-			resContent = "안녕하세요>_<";
-		}else if(reqContent.startsWith("!날씨")) {
+			resContent = "안녕하세요~ :)";
+		}else if(reqContent.equals("도움말")){
+			resContent = "[도움말]";
+			resContent += "\r1. 날씨";
+			resContent += "\rEX.!날씨, !날씨 서울, !날씨 부산";
+			resContent += "\r\r2. 키워드 검색";
+			resContent += "\rEX. !검색 심심이";
+		}
+		else if(reqContent.startsWith("!날씨")) {
 			if(reqContent.contains("서울"))	resContent = "현재 서울은 "+weatherService.getCertainWeather("seoul")+"입니다. :)";
 			else if(reqContent.contains("인천"))	resContent = "현재 인천은 "+weatherService.getCertainWeather("incheon")+"입니다. :)";
 			else if(reqContent.contains("춘천"))	resContent = "현재 춘천은 "+weatherService.getCertainWeather("chuncheon")+"입니다. :)";
@@ -53,26 +60,6 @@ public class ChatbotController {
 			else if(reqContent.contains("제주"))	resContent = "현재 제주는 "+weatherService.getCertainWeather("jeju")+"입니다. :)";
 			else	resContent = "전국 날씨를 알려드려요~"+weatherService.getWeathers();
 		}else if(reqContent.startsWith("!검색")) {
-			/*
-			■ 키워드 : 겨울여행
-			└ 피　씨 : 4,950 회
-			└ 모바일 : 11,700 회
-			└ 합　계 : 16,650 회
-			└ 블로그 : 229,761 건
-			└ 순　위 : n, n, n, n, n, n, n, n, n, n
-
-			■ 연관키워드
-			└ 제주도 겨울
-			└ 국내 여행지 추천
-			└ 12월 제주도
-			└ 1박2일 국내여행
-			└ 여수 겨울여행
-			└ 제주도 12월 여행
-			└ 국내 1박2일 겨울여행
-			└ 제주도 겨울여행코스
-			└ 겨울 제주
-			└ 제주도 가을
-			*/
 			String[] searchWord = reqContent.split(" ");
 			HashMap<String, Object> resultDataFromBlog = searchWordService.getSearchWordFromBlog(searchWord[1]);
 			DecimalFormat commas = new DecimalFormat("#,###");
@@ -87,8 +74,10 @@ public class ChatbotController {
 			resContent += searchWordService.getRelatedSearches(searchWord[1]);
 
 		}else {
-			resContent = "이해가 가지 않습니다. 다시 한번 얘기해주세요~";
+			resContent = "이해가 가지 않습니다.";
+			resContent += "\r[도움말]을 입력하시면, 사용법을 확인하실 수 있어요~";
 		}
+		
 		MessageVO mes = new MessageVO(resContent);
 		ChatResponseVO res = new ChatResponseVO();
 		res.setMessage(mes);
--- HyoniCrawling/src/main/java/com/hyoni/crawling/dao/SearchWordDao.java
+++ HyoniCrawling/src/main/java/com/hyoni/crawling/dao/SearchWordDao.java
@@ -3,7 +3,6 @@ package com.hyoni.crawling.dao;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -14,7 +13,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.junit.Test;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -82,7 +82,6 @@ public class SearchWordDao {
 		final String clientId = "0f5PGUtgYLnO09cq5LpN";// 애플리케이션 클라이언트 아이디값";
 		final String clientSecret = "XvTpNcCebC";// 애플리케이션 클라이언트 시크릿값";
 		String searchedData = null;
-		HashMap<String, Object> resultData = new HashMap<>();	
 		
 		/*검색API 호출*/
 		try {
@@ -114,20 +113,28 @@ public class SearchWordDao {
 		return obj.getInt("total");
 	}
 	
-	@Test
-	public void getRelatedSearches() throws UnsupportedEncodingException {
+	public String getRelatedSearches(String searchWord) {
+		String connectUrl = "https://search.naver.com/search.naver?where=nexearch&sm=tab_jum&query=";
+		String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
+		String relatedSearches = "";
+		
 		Document doc = null;
-		String text = URLEncoder.encode("한국시리즈", "UTF-8");
+		Elements items = null;
 		try {
-			doc = Jsoup.connect("https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query="+text).get();
+			doc = Jsoup.connect(connectUrl+URLEncoder.encode(searchWord, "UTF-8"))
+					.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+					.header("Accept-encoding", "gzip, deflate, br")
+					.header("Accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
+					.header("Cache-control", "max-age=0")
+					.header("Cookie", "NNB=6OFX6PLLOZLFS; npic=r8a+J0JV4FaSmBht0wpBrEOt4KewcPWSpth6qRRg0nJ7LsmcLCsdOt/IOTadVdcCCA==; PCID=15007847098034900797112; _ga=GA1.2.78581027.1500784703; ASID=73a104bf0000015d918bd3200000004b; nx_open_so=1; nx_ssl=2; BMR=s=1541822086278&r=https%3A%2F%2Fm.blog.naver.com%2Foccidere%2F220851125347&r2=https%3A%2F%2Fwww.google.co.kr%2F; page_uid=T+ijEdpVuE8ssvWMiKRssssstvd-227893; _naver_usersession_=smXnTb4LYU8T4vnUweZA2w==")
+					.header("Upgrade-insecure-requests", "1")
+					.userAgent(userAgent).get();
+			items = doc.select("._related_keyword_ul").select("li");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println(doc);
-	}
-
-	public String getRelatedSearches(String searchWord) {
-		return null;
+		for (Element item : items)	relatedSearches += "\r└ "+item.text();
+		return relatedSearches;
 	}
 }
